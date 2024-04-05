cccd28cbae1257fc3e4d77be497c994d26a60aac
Szeng89 <shijinz898@gmail.com>
2024-04-04 23:38:19 -0400

    Did cat tests

---
+++ src/main/java/Cat.java
@@ -0,0 +1,22 @@
+public class Cat extends Pet{
+    public Cat(int age, String name) {
+        super(age, name);
+    }
+
+    public Cat() {
+        super();
+    }
+
+    public Cat(int age) {
+        super(age);
+    }
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String speak() {
+        return "Meow!";
+    }
+}
---
+++ src/test/java/CatTest.java
@@ -0,0 +1,45 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CatTest {
+    @Test
+    public void constructorNameAge() {
+        Cat cat = new Cat(11, "Sky");
+
+        Assert.assertEquals("Sky", cat.getName());
+        Assert.assertEquals(11, cat.getAge());
+    }
+
+    @Test
+    public void nullConstructorTest() {
+        Cat cat = new Cat();
+
+        Assert.assertNotNull(cat);
+    }
+
+    @Test
+    public void constructorAgeTest() {
+        int age = 11;
+        Cat cat = new Cat(age);
+
+        Assert.assertEquals(age, cat.getAge());
+    }
+
+    @Test
+    public void constructorNameTest() {
+        String name = "Davvid";
+        Cat cat = new Cat(name);
+
+        Assert.assertEquals(name, cat.getName());
+    }
+
+    @Test
+    public void speakTest() {
+        Cat cat = new Cat(11,"ralphy");
+
+        String expected = "Meow!";
+        String actual = cat.speak();
+
+        Assert.assertEquals(expected, actual);
+    }
+}
