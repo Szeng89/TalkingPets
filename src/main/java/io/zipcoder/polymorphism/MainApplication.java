a0d231b91e6932dc1982f802f9ec03d8e0917577
Szeng89 <shijinz898@gmail.com>
2024-04-04 22:59:02 -0400

    did part 1

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,37 @@
 package io.zipcoder.polymorphism;
 
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! Do you have any pets?");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Surely you jest, How many pets do you have?");
+            scanner.next();
+        }
+        int howManyPets = scanner.nextInt();
+
+        ArrayList<String> petName = new ArrayList<>();
+        ArrayList<String> kindOfPet = new ArrayList<>();
+
+        scanner.nextLine();
+        for (int i = 1; i <= howManyPets; i ++) {
+            System.out.println("Whats the name of pet " + i + "?");
+            String nameOfPets = scanner.nextLine();
+            petName.add(nameOfPets);
+
+            System.out.println("What kind of pet is pet " + i + "?");
+            String whatKindOfPet = scanner.nextLine();
+            kindOfPet.add(whatKindOfPet);
+        }
+        System.out.println(kindOfPet + " named " + petName);
+
+    }
+
+
 }
